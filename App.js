497b1bb8ca642d2d96960402be77142c08b3485c
AbdulHaseeb2289 <fearforu2002@gmail.com>
2024-08-27 22:06:21 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from 'src./components/Header';
+import Menu from 'src./components/Menu';
+import Footer from 'src./components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
